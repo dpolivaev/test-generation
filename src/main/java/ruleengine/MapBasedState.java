8ecb725252d4186ccb92b2de56d1c479732a2865
dpolivaev <dpolivaev>
2013-07-17 23:11:43 +0200

    tests + optimization for strategy

--- src/main/java/ruleengine/MapBasedState.java
+++ src/main/java/ruleengine/MapBasedState.java
@@ -1,5 +1,7 @@
 package ruleengine;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -8,12 +10,14 @@ public class MapBasedState {
 	private StringBuilder assignedPropertiesAsString = new StringBuilder();
 	private int count = 0;
 	private Map<String, Object> properties = new LinkedHashMap<String, Object>();
+    private Collection<Rule> firedRules = new ArrayList<>();
 
 	public void setPropertyValue(Rule rule, Object value) {
 		String targetedPropertyName = rule.getTargetedPropertyName();
         if (properties.containsKey(targetedPropertyName))
             throw new PropertyAlreadyAssignedException();
         properties.put(targetedPropertyName, value);
+        firedRules.add(rule);
 		if (assignedPropertiesAsString.length() > 0)
 			assignedPropertiesAsString.append('\t');
 		this.assignedPropertiesAsString.append(rule.getTargetedPropertyName())
@@ -23,6 +27,7 @@ public class MapBasedState {
 	public void nextCombination() {
 		this.count++;
         properties.clear();
+        firedRules.clear();
 		assignedPropertiesAsString.setLength(0);
 
 	}
@@ -42,5 +47,9 @@ public class MapBasedState {
     public Object get(String name) {
 		return properties.get(name);
 	}
+
+    public Collection<Rule> firedRules() {
+        return firedRules;
+    }
 
 }
