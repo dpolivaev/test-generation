8da341e5fd5733f9d12ff713eccc499e0756c291
dpolivaev <dpolivaev@gmx.de>
2013-09-01 18:57:16 +0200

    Move test methods to AssignmentsTest

--- src/test/java/ruleengine/impl/AssignmentsTest.java
+++ src/test/java/ruleengine/impl/AssignmentsTest.java
@@ -1,19 +1,81 @@
 package ruleengine.impl;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static ruleengine.RuleBuilder.Factory.iterate;
+import static testutils.TestUtils.set;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import ruleengine.AssignmentFormatter;
 import ruleengine.SpecialValue;
 import ruleengine.impl.Assignments;
 
 public class AssignmentsTest {
 
+	Assignments assignments;
+
+    private StatefulRule ruleForProperty(String propertyName) {
+        return iterate(propertyName).asRule();
+    }
+
+
+	@Before
+	public void setup() {
+		assignments = new Assignments();
+	}
+
     @Test
     public void returnsUndefinedForNotAssignedProperty() {
-        Assignments assignments = new Assignments();
         assertThat(assignments.get("unknown"), equalTo((Object)SpecialValue.UNDEFINED));
     }
+
+	@Test
+	public void stateWithOnePropertySetAtFirstCombination() {
+		clearAssignments();
+        assignments.add(new Assignment(ruleForProperty("x"), "a",
+            ""));
+        String expectedScriptPropertyCombinations = "x=a";
+		assertEquals(expectedScriptPropertyCombinations,
+            AssignmentFormatter.create("=", ", ").format(assignments));
+	}
+
+
+    private void clearAssignments() {
+        assignments = new Assignments();
+    }
+
+	@Test
+	public void stateWithOneProperty_containsItsValue() {
+		clearAssignments();
+        assignments.add(new Assignment(ruleForProperty("x"), "a",
+            ""));
+		assertThat(assignments.containsProperties(set("x")), equalTo(true));
+	}
+
+	@Test
+	public void stateWithTwoPropertiesSetAtFirstCombination() {
+		clearAssignments();
+        assignments.add(new Assignment(ruleForProperty("x"), "a",
+            ""));
+        assignments.add(new Assignment(ruleForProperty("y"), "b",
+            ""));
+        String expectedScriptPropertyCombinations = "x=a, y=b";
+		assertEquals(expectedScriptPropertyCombinations,
+		AssignmentFormatter.create("=", ", ").format(assignments));
+	}
+
+
+    @Test
+    public void oldPropertiesAreRemovedAfterIterationEnd() {
+        clearAssignments();
+        assignments.add(new Assignment(ruleForProperty("x"), "a",
+            ""));
+        clearAssignments();
+        assertThat(assignments.containsProperties(set("x")), equalTo(false));
+    }
+
 
 }
