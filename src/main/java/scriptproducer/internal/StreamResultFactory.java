8c56e5eb6abceb9a02e40318fa7423045cf12074
dpolivaev <dpolivaev>
2013-09-02 18:46:01 +0200

    support arbitrary file extensions allow use of predefined xslt sheets from class path resources

--- src/main/java/scriptproducer/internal/StreamResultFactory.java
+++ src/main/java/scriptproducer/internal/StreamResultFactory.java
@@ -6,14 +6,21 @@ import javax.xml.transform.Result;
 import javax.xml.transform.stream.StreamResult;
 
 public class StreamResultFactory implements ResultFactory{
-    private File baseDirectory;
-    public File getBaseDirectory() {
+    private File baseDirectory = null;
+	private String fileExtension = "xml";
+    public String getFileExtension() {
+		return fileExtension;
+	}
+	public void setFileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+	}
+	public File getBaseDirectory() {
         return baseDirectory;
     }
     public void setBaseDirectory(File baseDir) {
         this.baseDirectory = baseDir;
     }
     public Result newResult(String scriptName) {
-        return new StreamResult(new File(baseDirectory, scriptName + ".xml"));
+		return new StreamResult(new File(baseDirectory, scriptName + "." + fileExtension));
     }
 }
