99b59cd6c5325821d74bbc46fc80f3adba667faf
dpolivaev <dpolivaev@gmx.de>
2013-09-02 18:46:01 +0200

    support arbitrary file extensions allow use of predefined xslt sheets from class path resources

--- src/main/java/scriptproducer/StrategyRunner.java
+++ src/main/java/scriptproducer/StrategyRunner.java
@@ -1,9 +1,12 @@
 package scriptproducer;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 
 import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
 import ruleengine.RuleEngine;
 import ruleengine.Strategy;
@@ -14,6 +17,15 @@ import utils.Utils;
 
 public class StrategyRunner {
 	private Source xsltSource = null;
+	private String fileExtension = "xml";
+	public String getFileExtension() {
+		return fileExtension;
+	}
+
+	public void setFileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+	}
+
 	public void run(Strategy strategy){
 		RuleEngine ruleEngine = new RuleEngine();
 		OutputStreamWriter writer = new OutputStreamWriter(System.out);
@@ -21,7 +33,9 @@ public class StrategyRunner {
 		ruleEngine.addScriptProducer(logger);
 		ruleEngine.addErrorHandler(logger);
 		StreamResultFactory resultFactory = new StreamResultFactory();
+		resultFactory.setFileExtension(fileExtension);
 		MultipleScriptsProducer scriptProducer = new MultipleScriptsProducer(resultFactory);
+		scriptProducer.setXsltSource(xsltSource);
 		ruleEngine.addScriptProducer(scriptProducer);
 		ruleEngine.run(strategy);
 		try {
@@ -35,10 +49,20 @@ public class StrategyRunner {
 	public Source getXsltSource() {
 		return xsltSource;
 	}
-	public StrategyRunner apply(Source xsltSource) {
+	public StrategyRunner apply(Source xsltSource, String fileExtension) {
 		this.xsltSource = xsltSource;
+		this.fileExtension = fileExtension;
 		return this;
 	}
-	
+
+	public StrategyRunner apply(String xsltSource, String fileExtension) {
+		File xsltFile = new File(xsltSource);
+		if(xsltFile.canRead())
+			return apply(new StreamSource(xsltFile), fileExtension);
+		InputStream resource = getClass().getResourceAsStream(xsltSource);
+		if(resource != null)
+			return apply(new StreamSource(resource), fileExtension);
+		throw new IllegalArgumentException("source " + xsltSource + " not available");
+	}
 }
 
