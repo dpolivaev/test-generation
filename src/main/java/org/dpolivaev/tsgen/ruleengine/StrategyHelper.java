a3985389bd7cde425f928a079e03e6cd459dd8b4
dpolivaev <dpolivaev>
2013-09-14 12:20:04 +0200

    DescriptionProvider strategy

--- src/main/java/org/dpolivaev/tsgen/ruleengine/StrategyHelper.java
+++ src/main/java/org/dpolivaev/tsgen/ruleengine/StrategyHelper.java
@@ -1,11 +1,16 @@
 package org.dpolivaev.tsgen.ruleengine;
 
+import org.dpolivaev.tsgen.ruleengine.internal.DescriptionProvider;
 import org.dpolivaev.tsgen.ruleengine.internal.TestIdProvider;
 
 public class StrategyHelper {
 
 	public static Strategy id(String propertyName) {
 		return TestIdProvider.strategy(propertyName);
+	}
+	
+	public static Strategy description(String propertyName) {
+		return DescriptionProvider.strategy(propertyName);
 	}
 	
 }
---
+++ src/test/java/org/dpolivaev/tsgen/ruleengine/internal/DescriptionProviderTest.java
@@ -0,0 +1,26 @@
+package org.dpolivaev.tsgen.ruleengine.internal;
+
+import static org.dpolivaev.tsgen.testutils.TestUtils.ruleMock;
+
+import org.dpolivaev.tsgen.ruleengine.PropertyContainer;
+import org.dpolivaev.tsgen.ruleengine.Rule;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DescriptionProviderTest {
+
+	@Test
+	public void test() {
+        Rule rule1 = ruleMock("name");
+        Rule rule2 = ruleMock("name2");
+        
+        Assignments assignments = new Assignments();
+        assignments.add(new Assignment(rule1, "value", ""));
+        assignments.add(new Assignment(rule2, "value2", "name->"));
+        
+		String description = new DescriptionProvider().describe((PropertyContainer)assignments);
+		Assert.assertEquals("name: value\n"+
+						"name->name2: value2", description);
+	}
+
+}
