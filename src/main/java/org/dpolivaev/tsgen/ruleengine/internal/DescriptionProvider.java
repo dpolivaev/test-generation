97423e64019476027e8ae129d5b6aba05d2a703c
dpolivaev <dpolivaev@gmx.de>
2013-09-14 12:20:04 +0200

    DescriptionProvider strategy

---
+++ src/main/java/org/dpolivaev/tsgen/ruleengine/internal/DescriptionProvider.java
@@ -0,0 +1,39 @@
+package org.dpolivaev.tsgen.ruleengine.internal;
+
+import org.dpolivaev.tsgen.ruleengine.AssignmentFormatter;
+import org.dpolivaev.tsgen.ruleengine.PropertyContainer;
+import org.dpolivaev.tsgen.ruleengine.RuleBuilder;
+import org.dpolivaev.tsgen.ruleengine.Strategy;
+import org.dpolivaev.tsgen.ruleengine.ValueProvider;
+
+public class DescriptionProvider  implements ValueProvider{
+
+	private AssignmentFormatter formatter;
+
+	public DescriptionProvider() {
+		this(": ", "\n");
+	}
+
+	public DescriptionProvider(String nameValueSeparator, String propertySeparator) {
+		formatter = AssignmentFormatter.create(nameValueSeparator, propertySeparator);
+		formatter.exclude("requirement\\.");
+		formatter.appendReasons(true);
+	}
+
+	public String describe(PropertyContainer assignments) {
+		return formatter.format(assignments);
+	}
+
+	public static Strategy strategy(String propertyName){
+		Strategy strategy = new Strategy();
+		DescriptionProvider instance = new DescriptionProvider(": ", "\n");
+		strategy.addDefaultRule(RuleBuilder.Factory.iterate(propertyName).over(instance));
+		return strategy;
+
+	}; 
+	
+	@Override
+	public Object value(PropertyContainer propertyContainer) {
+		return describe(propertyContainer);
+	}
+}
