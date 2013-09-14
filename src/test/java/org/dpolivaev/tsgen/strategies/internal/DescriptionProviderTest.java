7b408bd7933305a3041c6aebe86d0f77d40f5578
dpolivaev <dpolivaev>
2013-09-14 12:49:57 +0200

    description provider excludes requirements

--- src/test/java/org/dpolivaev/tsgen/strategies/internal/DescriptionProviderTest.java
+++ src/test/java/org/dpolivaev/tsgen/strategies/internal/DescriptionProviderTest.java
@@ -13,17 +13,30 @@ import org.junit.Test;
 public class DescriptionProviderTest {
 
 	@Test
-	public void test() {
+	public void includesUsualRulesWithReasons() {
         Rule rule1 = ruleMock("name");
         Rule rule2 = ruleMock("name2");
+        Rule ruleRequirement = ruleMock("requirement.id1");
         
         Assignments assignments = new Assignments();
         assignments.add(new Assignment(rule1, "value", ""));
         assignments.add(new Assignment(rule2, "value2", "name->"));
+        assignments.add(new Assignment(ruleRequirement, "reason", ""));
         
 		String description = new DescriptionProvider().describe((PropertyContainer)assignments);
 		Assert.assertEquals("name: value\n"+
 						"name->name2: value2", description);
+	}
+
+	@Test
+	public void excludesRequirements() {
+        Rule ruleRequirement = ruleMock("requirement.id1");
+        
+        Assignments assignments = new Assignments();
+        assignments.add(new Assignment(ruleRequirement, "reason", ""));
+        
+		String description = new DescriptionProvider().describe((PropertyContainer)assignments);
+		Assert.assertEquals("", description);
 	}
 
 }
