1e114e5f4547cc9e2187ca94dce76e1aac399647
dpolivaev <dpolivaev>
2013-09-21 12:34:08 +0200

    RuleEngine registers goals and executes goal checks

--- src/test/java/org/dpolivaev/tsgen/ruleengine/RuleEngineTest.java
+++ src/test/java/org/dpolivaev/tsgen/ruleengine/RuleEngineTest.java
@@ -1,26 +1,37 @@
 package org.dpolivaev.tsgen.ruleengine;
 import static org.dpolivaev.tsgen.ruleengine.RuleBuilder.Factory.iterate;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import org.dpolivaev.tsgen.coverage.CoverageTracker;
+import org.dpolivaev.tsgen.coverage.Goal;
+import org.dpolivaev.tsgen.coverage.internal.RequirementCoverage;
 import org.dpolivaev.tsgen.ruleengine.PropertyContainer;
 import org.dpolivaev.tsgen.ruleengine.RuleEngine;
 import org.dpolivaev.tsgen.ruleengine.PropertyHandler;
 import org.dpolivaev.tsgen.ruleengine.Strategy;
 import org.dpolivaev.tsgen.ruleengine.internal.StatefulRule;
 import org.dpolivaev.tsgen.utils.internal.Utils;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 
 public class RuleEngineTest {
+	
+	private Strategy strategy;
+	private RuleEngine ruleEngine;
+
+	@Before
+	public void setup(){
+		strategy = Mockito.mock(Strategy.class);
+		ruleEngine = new RuleEngine();
+	}
 
 	@Test
 	public void containsAllDefaultProperties() {
-		Strategy strategy = Mockito.mock(Strategy.class);
 		Mockito.when(strategy.availableDefaultProperties()).thenReturn(Utils.set("name"));
-		final RuleEngine ruleEngine = new RuleEngine().addScriptWriter(new PropertyHandler() {
+		ruleEngine.addScriptWriter(new PropertyHandler() {
 			
 			@Override
 			public void handlePropertyCombination(PropertyContainer propertyContainer, CoverageTracker coverage) {
@@ -33,8 +44,7 @@ public class RuleEngineTest {
 
     @Test
 	public void ruleEngineResetsAssignmentReasonAfterPropertySetEventIsFired() {
-		Strategy strategy = Mockito.mock(Strategy.class);
-		final RuleEngine ruleEngine = new RuleEngine().addScriptWriter(new PropertyHandler() {
+		ruleEngine.addScriptWriter(new PropertyHandler() {
 			
 			@Override
 			public void handlePropertyCombination(PropertyContainer propertyContainer, CoverageTracker coverage) {
@@ -46,5 +56,14 @@ public class RuleEngineTest {
 			}
 		});
 		ruleEngine.run(strategy);
+	}
+    
+    @Test
+	public void ruleEngineExecutesGoalChecks() throws Exception {
+		Goal goal = Mockito.mock(Goal.class);
+		ruleEngine.addGoal(goal);
+		ruleEngine.run(strategy);
+		
+		Mockito.verify(goal).check(ruleEngine, ruleEngine.coverage());
 	}
 }
