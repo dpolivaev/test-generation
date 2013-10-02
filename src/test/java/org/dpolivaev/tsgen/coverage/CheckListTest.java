a61133c1b17a0547419d746b997757cb2c8f6229
dpolivaev <dpolivaev@gmx.de>
2013-10-02 18:36:16 +0200

    group tests

--- src/test/java/org/dpolivaev/tsgen/coverage/CheckListTest.java
+++ src/test/java/org/dpolivaev/tsgen/coverage/CheckListTest.java
@@ -1,17 +1,39 @@
 package org.dpolivaev.tsgen.coverage;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CheckListTest {
 
 	private static final CoverageEntry COVERAGE_ENTRY = new CoverageEntry("goal", "reason");
 
+	private CheckList checkList;
+	
+	@Before
+	public void setup(){
+		checkList = new CheckList();
+	}
+
+	@Test
+	public void coverageEntryEquality() {
+		final CoverageEntry coverageEntry1 = new CoverageEntry("req", "reason");
+		final CoverageEntry coverageEntry2 = new CoverageEntry("req", "reason");
+		assertThat(coverageEntry1, equalTo(coverageEntry2));
+		assertThat(coverageEntry1.hashCode(), equalTo(coverageEntry2.hashCode()));
+	}
+	
+	@Test
+	public void afterOneEntryIsAdded_coverageCountIs1() throws Exception {
+		checkList.addReached(COVERAGE_ENTRY);
+		assertThat(checkList.countReached(COVERAGE_ENTRY), equalTo(1));
+	}
+
 	@Test
 	public void addsItem() {
-		final CheckList checkList = new CheckList();
 		checkList.addReached(COVERAGE_ENTRY);
 		
 		assertThat(checkList.countReached(COVERAGE_ENTRY), CoreMatchers.equalTo(1));
@@ -19,7 +41,6 @@ public class CheckListTest {
 
 	@Test
 	public void putsItem() {
-		final CheckList checkList = new CheckList();
 		checkList.setReached(COVERAGE_ENTRY, 3);
 		
 		assertThat(checkList.countReached(COVERAGE_ENTRY), CoreMatchers.equalTo(3));
