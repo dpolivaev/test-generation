eb388fedf8b50a020a1c150ab87aa4a9be5b6742
dpolivaev <dpolivaev>
2013-11-03 10:44:10 +0100

    refactor test

--- src/test/java/org/dpolivaev/tsgen/scriptwriter/StrategyRunnerTest.java
+++ src/test/java/org/dpolivaev/tsgen/scriptwriter/StrategyRunnerTest.java
@@ -10,6 +10,20 @@ import org.dpolivaev.tsgen.strategies.StrategyHelper;
 import org.junit.Test;
 
 public class StrategyRunnerTest {
+	private void checkOutputFilesAreCreated(final File expectedOutputFile,
+			final File expectedReportFile) {
+		assertThat(expectedOutputFile.canRead(), equalTo(true));
+		assertThat(expectedReportFile.canRead(), equalTo(true));
+	}
+
+	private void checkOutputStreamsAreClosed(final File expectedOutputFile,
+			final File expectedReportFile) {
+		expectedOutputFile.delete();
+		expectedReportFile.delete();
+		assertThat(expectedOutputFile.canRead(), equalTo(false));
+		assertThat(expectedReportFile.canRead(), equalTo(false));
+	}
+	
 	@Test
 	public void outputsXml() throws Exception {
 		Strategy strategy = new Strategy();
@@ -19,12 +33,10 @@ public class StrategyRunnerTest {
 		expectedReportFile.delete();
 		final StrategyRunner strategyRunner = new StrategyRunner();
 		strategyRunner.getOutputConfiguration().setFileDirectory("testoutput").setFileExtension("xml");
+		
 		strategyRunner.run(strategy.with(StrategyHelper.id("testcase")));
-		assertThat(expectedOutputFile.canRead(), equalTo(true));
-		assertThat(expectedReportFile.canRead(), equalTo(true));
-		expectedOutputFile.delete();
-		expectedReportFile.delete();
-		assertThat(expectedOutputFile.canRead(), equalTo(false));
-		assertThat(expectedReportFile.canRead(), equalTo(false));
+		
+		checkOutputFilesAreCreated(expectedOutputFile, expectedReportFile);
+		checkOutputStreamsAreClosed(expectedOutputFile, expectedReportFile);
 	}
 }
