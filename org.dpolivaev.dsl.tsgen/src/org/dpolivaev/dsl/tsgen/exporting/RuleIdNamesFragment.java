1f356de8878bbdb5a5e706929e9eaa94a1c3fc78
dpolivaev <dpolivaev@gmx.de>
2013-12-29 19:49:37 +0100

    fix bugs

--- org.dpolivaev.dsl.tsgen/src/org/dpolivaev/dsl/tsgen/exporting/RuleIdNamesFragment.java
+++ org.dpolivaev.dsl.tsgen/src/org/dpolivaev/dsl/tsgen/exporting/RuleIdNamesFragment.java
@@ -5,8 +5,8 @@ import org.dpolivaev.dsl.tsgen.naming.ValuesXBaseNameProvider;
 import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.generator.BindFactory;
 import org.eclipse.xtext.generator.Binding;
-import org.eclipse.xtext.generator.DefaultGeneratorFragment;
 import org.eclipse.xtext.generator.IGeneratorFragment;
+import org.eclipse.xtext.generator.exporting.QualifiedNamesFragment;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 
 /**
@@ -15,20 +15,12 @@ import org.eclipse.xtext.naming.IQualifiedNameProvider;
  * @author Sven Efftinge - Initial contribution and API
  * @author Sebastian Zarnekow
  */
-public class RuleIdNamesFragment extends DefaultGeneratorFragment {
+public class RuleIdNamesFragment extends QualifiedNamesFragment {
 
 	@Override
 	public Set<Binding> getGuiceBindingsRt(Grammar grammar) {
 		return new BindFactory()
 			.addfinalTypeToType(IQualifiedNameProvider.class.getName(), ValuesXBaseNameProvider.class.getName())
 			.getBindings();
-	}
-
-	@Override
-	public Set<Binding> getGuiceBindingsUi(Grammar grammar) {
-		return new BindFactory()
-		.addTypeToType("org.eclipse.xtext.ui.refactoring.IDependentElementsCalculator",
-				"org.eclipse.xtext.ui.refactoring.impl.DefaultDependentElementsCalculator")
-		.getBindings();
 	}
 }
