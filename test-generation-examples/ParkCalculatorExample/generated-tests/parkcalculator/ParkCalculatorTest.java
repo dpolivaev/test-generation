e0c253f700c761a17e616788e306a29cb83fad41
dpolivaev <dpolivaev@gmx.de>
2014-04-15 12:15:52 +0200

    adapt example projects to new syntax

--- test-generation-examples/ParkCalculatorExample/generated-tests/parkcalculator/ParkCalculatorTest.java
+++ test-generation-examples/ParkCalculatorExample/generated-tests/parkcalculator/ParkCalculatorTest.java
@@ -9,8 +9,8 @@ import org.dpolivaev.tsgen.java.Coverage;
 import org.dpolivaev.tsgen.java.GoalCoverage;
 
 import static parkcalculator.Lots.*;
-import static parkcalculator.BadValues.*;
-import parkcalculator.testgeneration.LongTermGarageParkingCalculator;
+	import static parkcalculator.BadValues.*;
+	import parkcalculator.testgeneration.LongTermGarageParkingCalculator;
 import parkcalculator.ParkCalculatorTestDriver;
 import static parkcalculator.ParkCalculatorTestDriver.*;
 
