2680f6e721a0a67f47afb06b4df7efb2db245224
dpolivaev <dpolivaev@gmx.de>
2014-10-31 15:19:04 +0100

    dynamic type recognition in ValueProviderHelper

--- test-generation-engine/src/main/java/org/dpolivaev/testgeneration/engine/ruleengine/ValueProviderHelper.java
+++ test-generation-engine/src/main/java/org/dpolivaev/testgeneration/engine/ruleengine/ValueProviderHelper.java
@@ -2,7 +2,9 @@ package org.dpolivaev.testgeneration.engine.ruleengine;
 
 public class ValueProviderHelper {
     public static Object toValue(final Object value, final PropertyContainer propertyContainer) {
-        return value;
+    	if (value instanceof ValueProvider)
+    		return toValue((ValueProvider)value, propertyContainer);
+    	return value;
       }
 
     public static Object toValue(final ValueProvider valueProvider, final PropertyContainer propertyContainer) {
